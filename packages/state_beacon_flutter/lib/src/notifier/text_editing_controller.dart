94f4b15d71d69b7ffdfb9988832381c501e5963d
zupat <starboymc4@gmail.com>
2026-04-27 13:47:18 +0700

    add getTextEditingController to writeable beacons

--- packages/state_beacon_flutter/lib/src/notifier/text_editing_controller.dart
+++ packages/state_beacon_flutter/lib/src/notifier/text_editing_controller.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:state_beacon_core/state_beacon_core.dart';
 
+final Map<int, _TextEditingController> _controllerCache = {};
+
 /// This is a wrapper around a [TextEditingController] that
 /// allows you to hook into the controller's lifecycle.
 class _TextEditingController extends TextEditingController {
@@ -78,5 +80,50 @@ class TextEditingBeacon extends WritableBeacon<TextEditingValue> {
 
     _controller.dispose();
     super.dispose();
+  }
+}
+
+/// Extensions for [WritableBeacon] to work with [TextEditingController].
+extension WritableBeaconTextEditingController on WritableBeacon<String> {
+  /// Returns a [TextEditingController] that is synced with this beacon.
+  TextEditingController getTextEditingController() {
+    final key = hashCode;
+    final cached = _controllerCache[key];
+
+    if (cached != null) return cached;
+
+    final controller = _TextEditingController();
+    _controllerCache[key] = controller;
+
+    var syncing = false;
+
+    void safeWrite(VoidCallback fn) {
+      if (syncing) return;
+      syncing = true;
+      try {
+        fn();
+      } finally {
+        syncing = false;
+      }
+    }
+
+    controller
+      ..text = peek()
+      ..addListener(() {
+        safeWrite(() => set(controller.text));
+      });
+
+    final unsub = subscribeSynchronously(
+      (v) => safeWrite(() => controller.text = v),
+    );
+
+    controller.disposeCallback = () {
+      unsub();
+      _controllerCache.remove(key);
+    };
+
+    onDispose(controller.dispose);
+
+    return controller;
   }
 }
